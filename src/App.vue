6e5bfabd4c2e4f2016a1d00517c9a273dc152930
Florian <florian.berrot92@gmail.com>
2020-01-29 10:20:00 +0100

    add vue project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <h3 v-if="avg === 3">
+      OUAIP C'EST 3 GROOOOS
+    </h3>
+    <h3 v-else>
+      PRESQUE !!!!!
+    </h3>
+    <counter
+      :value="btn1"
+      @input="btn1=$event"
+    />
+    <counter
+      :value="btn2"
+      @input="btn2=$event"
+    />
+    <counter v-model="btn3" />
+    <p>Moyenne : {{ avg }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import counter from './components/Counter.vue';
+
+const createCounters = () => ({
+  btn1: 0,
+  btn2: 0,
+  btn3: 0,
+});
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld,
+    counter,
+  },
+  data() {
+    return {
+      btn1: 0,
+      btn2: 0,
+      btn3: 0,
+    };
+  },
+  computed: {
+    avg() {
+      return (this.btn1 + this.btn2 + this.btn3) / 3;
+    },
+  },
+  methods: {
+    yeyItWorks() {
+      alert('it works');
+    },
+    resetCounters() {
+      this.counter = createCounters();
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,35 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="emitEvent">
+      Click here
+    </button>
+    <br>    <br>
+    <div>
+      <button @click="eventPlus1">
+        +
+      </button>
+      <button @click="eventMoins1">
+        -
+      </button>
+      <p>valeur {{ but1 }}</p>
+
+      <button @click="eventPlus2">
+        +
+      </button>
+      <button @click="eventMoins2">
+        -
+      </button>
+      <p>valeur {{ but2 }}</p>
+      <button @click="eventPlus3">
+        +
+      </button>
+      <button @click="eventMoins3">
+        -
+      </button>
+      <p>valeur {{ but3 }}</p>
+      <p>La moyenne de ces trois boutons est de : {{ moy }}</p>
+    </div>
   </div>
 </template>
 
@@ -34,7 +37,52 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
+    but1: {
+      type: Number,
+      required: true,
+    },
+    but2: {
+      type: Number,
+      required: true,
+    },
+    but3: {
+      type: Number,
+      required: true,
+    },
+    moy: {
+      type: Number,
+      required: true,
+    },
+    msg: {
+      type: String,
+      default: 'VueJS',
+    },
+  },
+  methods: {
+    emitEvent() {
+      this.$emit('slt');
+    },
+    eventPlus1() {
+      this.$emit('plus1');
+    },
+    eventPlus2() {
+      this.$emit('plus2');
+    },
+    eventPlus3() {
+      this.$emit('plus3');
+    },
+    eventMoins1() {
+      this.$emit('moins1');
+    },
+    eventMoins2() {
+      this.$emit('moins2');
+    },
+    eventMoins3() {
+      this.$emit('moins3');
+    },
+    averageSum() {
+      this.$emit('avg');
+    },
   },
 };
 </script>
